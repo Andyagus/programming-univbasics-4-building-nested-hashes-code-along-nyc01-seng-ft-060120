d86a6f4b45052e0463ac6daa92f02e0662da10f5
Andy Agus <redagus@gmail.com>
2020-05-22 01:36:25 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -11,14 +11,14 @@ epic_tragedy = {
       :hero => {
       name: "Romeo", 
       age: "15", 
-      status: "alive"},
+      status: "alive"
+      },
       :hero_friends => []
    },
    :capulet => {
       :patriarch => {
       name: "Lord Capulet", 
       age: "50"
-        
       },
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
